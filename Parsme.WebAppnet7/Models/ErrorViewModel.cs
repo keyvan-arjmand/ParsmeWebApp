2a8dba465f44da26aee8e02fa6fd565d891d5c83
Key1 <keyvan.arjmnd@gmail.com>
2024-12-09 18:47:57 +0330

    clone web app

---
+++ Parsme.WebAppnet7/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Parsme.WebAppnet7.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
